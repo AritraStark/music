121e3a4b8c9911c7e2fcba169801f6566489df64
AritraStark <58631915+AritraStark@users.noreply.github.com>
2020-07-24 01:51:14 +0530

    navbar animation added

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-console.log("hello");
+
 var i=1;
 var toggler= document.querySelector(".toggle");
 toggler.addEventListener("click",function(){
   if(i%2==0)
   {
     var a=document.querySelector(".nav2");
+    a.classList.add("display");
     a.classList.remove("hidden");
+
   }
   else{
     var a=document.querySelector(".nav2");
+    a.classList.remove("display");
     a.classList.add("hidden");
   }
   i++;
