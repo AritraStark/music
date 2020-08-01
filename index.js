b86aee48afa5e6db965a2ab77f8dcbc912a30f4d
AritraStark <58631915+AritraStark@users.noreply.github.com>
2020-08-02 03:43:51 +0530

    chatbot added stage1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
-var i=1;
+// responsive_toggler
+
+var i=2;
 var toggler= document.querySelector(".toggle");
 toggler.addEventListener("click",function(){
   if(i%2==0)
@@ -16,3 +18,35 @@ toggler.addEventListener("click",function(){
   }
   i++;
 });
+
+//chatbot
+var j=2;
+var startconv= document.querySelector(".chaton");
+startconv.addEventListener("click",function(){
+  if(j%2==0)
+  {
+    var b=document.querySelector(".chatcon");
+    b.classList.add("display");
+    b.classList.remove("hidden");
+
+  }
+  else{
+    var b=document.querySelector(".chatcon");
+    b.classList.remove("display");
+    b.classList.add("hidden");
+  }
+  j++;
+});
+
+//chatbot2
+var fsubm= document.querySelector(".startcon");
+fsubm.addEventListener("click",function(){
+
+    var c=document.querySelector(".chatcon2");
+    c.classList.add("display");
+    c.classList.remove("hidden");
+    var d=document.querySelector(".chatcon");
+    d.classList.remove("display");
+    d.classList.add("hidden");
+
+  });
